a697df00470bf5d08ec5e465c24c2752d473e094
Jon Bianco <web.projects@yahoo.com>
2018-07-20 23:31:49 -0700

    step 2 in process

---
+++ RestaurantBilling.UI/RestaurantBilling.UI/RestaurantBilling.UI.Android/Views/SplashScreenView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace RestaurantBilling.UI.Droid
+{
+    /// <summary>
+    /// MvvmCross Mods:
+    /// - Uses MvxSplashScreenActivity which will start the MvvmCross framework
+    /// - After init, will then use IMvxAppStart interface.
+    /// </summary>
+    [Activity(Label = "My App", MainLauncher = true, NoHistory = true, Icon = "@drawable/icon")]
+    public class SplashScreenActivity : MvxSplashScreenActivity
+    {
+        public SplashScreenActivity() : base(Resource.Layout.View_SplashScreen)
+        {
+
+        }
+    }
+}
